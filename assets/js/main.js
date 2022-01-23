0d4deca7339a7e8ccd804b4cfe51bc77a1972d4f
WebMakers <97015844+wethewebmakers@users.noreply.github.com>
2022-01-23 23:42:34 +0530

    Add files via upload (#1)
    * Add files via upload
    
    * Create sample.txt
    
    * Create style.css
    
    * Create sample.txt
    
    * Add files via upload
    
    * Create sample.txt
    
    * Add files via upload
    
    * Delete todo.txt
    
    * Delete sample.txt
    
    * Delete sample.txt
    
    * Delete sample.txt
    
    * Create sample.txt
    
    * Create sample.txt
    
    * Delete assets/vendor/animate.css directory
    
    * Update index.html
    
    * Create sample.txt
    
    * Add files via upload
    
    * Delete sample.txt
    
    * Create sample.txt
    
    * Add files via upload
    
    * Create sample.txt
    
    * Add files via upload
    
    * Create sample.txt
    
    * Add files via upload
    
    * Create sample.txt
    
    * Add files via upload
    
    * Add files via upload
    
    * Create sample.txt
    
    * Add files via upload
    
    * Create sample.txt
    
    * Add files via upload
    
    * Create sample.txt
    
    * Add files via upload
    
    * Create sample.txt
    
    * Add files via upload
    
    * Create sample.txt
    
    * Add files via upload
    
    * Create sample.txt
    
    * Add files via upload
    
    * Create sample.txt
    
    * Add files via upload
    
    * Delete sample.txt
    
    * Delete sample.txt
    
    * Delete sample.txt
    
    * Delete sample.txt
    
    * Delete sample.txt
    
    * Delete sample.txt
    
    * Delete sample.txt
    
    * Delete sample.txt
    
    * Delete sample.txt
    
    * Delete sample.txt
    
    * Delete sample.txt
    
    * Delete sample.txt

---
+++ assets/js/main.js
@@ -0,0 +1,192 @@
+/**
+* Template Name: Restaurantly - v3.7.0
+* Template URL: https://bootstrapmade.com/restaurantly-restaurant-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+(function() {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let header = select('#header')
+    let offset = header.offsetHeight
+
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth'
+    })
+  }
+
+  /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+  let selectHeader = select('#header')
+  let selectTopbar = select('#topbar')
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled')
+        if (selectTopbar) {
+          selectTopbar.classList.add('topbar-scrolled')
+        }
+      } else {
+        selectHeader.classList.remove('header-scrolled')
+        if (selectTopbar) {
+          selectTopbar.classList.remove('topbar-scrolled')
+        }
+      }
+    }
+    window.addEventListener('load', headerScrolled)
+    onscroll(document, headerScrolled)
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
+      }
+    }
+  });
+
+  /**
+   * Preloader
+   */
+  let preloader = select('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove()
+    });
+  }
+
+
+  /**
+   * Initiate glightbox 
+   */
+  const glightbox = GLightbox({
+    selector: '.glightbox'
+  });
+
+  /**
+   * Testimonials slider
+   */
+  new Swiper('.works-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      }
+    }
+  });
+
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
+  });
+
+})()
+
+function sendMail() {
+  user = document.getElementById('name').value;
+  mobile = document.getElementById('mobile').value;
+  email = document.getElementById('email').value;
+  message = document.getElementById('message').value;
+  document.getElementById('name').value = "";
+  document.getElementById('mobile').value = "";
+  document.getElementById('email').value = "";
+  document.getElementById('message').value = "";
+  
+  if(message=='' || user==''){
+    alert("Please fill in all the fields !!")
+  }else{
+    window.open("https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&source=mailto&to=mail2webmakers@gmail.com&su=Query from customer - Automatic mail&body=Hey, %0D%0A%0D%0AI am "+user+". I would like to ask:%0D%0A%0D%0A"+message+".%0D%0A%0D%0AContact No: "+mobile+ "%0D%0AEmail: "+email);
+  }
+}
+
+function showChat() {
+  document.getElementsByClassName('wa-chat-box')[0].style.display = 'block';
+}
+
+function closeChat() {
+  document.getElementsByClassName('wa-chat-box')[0].style.display = 'none';
+}
